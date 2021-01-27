e91a4815587489cc660148c27c7dc6ef68ecdb96
EC2 Default User <ec2-user@ip-172-31-17-119.ec2.internal>
2021-01-27 01:26:42 +0000

    add2

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -17,6 +17,21 @@ class TodolistsController < ApplicationController
     @list = List.find(params[:id])
   end
   
+  def edit 
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    #Listのfindメソッドでデータを見つけ、引数paramsで呼び出す
+    list = List.find(params[:id])
+      
+    #ローカル変数　paramsで送られたてきた値を受け取るメソッド
+    list.update(list_params)
+    
+    #show(詳細画面リダイレクト)
+    redirect_to todolist_path(list.id)
+  end
+  
   private
   def list_params
     params.require(:list).permit(:title,:body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  #HTTPメソッドが入る get post delete
+  #HTTPメソッドが入る get post(追加) patch(更新)　delete
   get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get 'todolists' => 'todolists#index'
   #以下は名前付きルート
   get 'todolists/:id' => 'todolists#show', as:'todolist'
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 end
