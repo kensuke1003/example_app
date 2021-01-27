594d1a84eb3f064b805d7e9cb3a462eedcfe0c5c
EC2 Default User <ec2-user@ip-172-31-17-119.ec2.internal>
2021-01-27 00:22:06 +0000

    add commit 1

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
   post 'todolists' =>'todolists#create'
+  get 'todolists' => 'todolists#index'
+  #以下は名前付きルート
+  get 'todolists/:id' => 'todolists#show', as:'todolist'
 end
